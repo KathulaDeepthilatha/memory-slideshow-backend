5772dda3cfe9389c6472038295fd3c752a611d8c
DeepthiLatha <kathuladeepthilatha9@gmail.com>
2025-06-24 13:24:12 +0530

    connected to mongodb atlas

--- server.js
+++ server.js
@@ -12,10 +12,7 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => console.log("MongoDB connected"))
+mongoose.connect(process.env.MONGO_URI).then(() => console.log("MongoDB connected"))
   .catch((err) => console.error("MongoDB error:", err));
 
 // Routes
